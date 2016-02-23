1eac2b02774e8818e880fb0f1565bec857c171e3
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-02-23 17:11:35 -0500

    Login UI & Service

---
+++ Connection/src/edu/nku/CSC440/stagbar/service/LoginService.java
@@ -0,0 +1,34 @@
+package edu.nku.CSC440.stagbar.service;
+
+public class LoginService {
+	public boolean login(String username, char[] password) {
+		System.out.println("Username: " + username);
+		System.out.println("Password: " + new String(password));
+
+		if(null == username) { return false; }
+
+		String passwordFromUser = new String(password);
+		String passwordFromDatabase = getPasswordForUser(username);
+
+		return null != passwordFromDatabase && passwordFromUser.equals(passwordFromDatabase);
+	}
+
+	/**
+	 * Searches database for given username.
+	 * If user is found, returns password for user.
+	 * Otherwise returns null.
+	 * In future, may return password hash for security.
+	 * @param username Username to search database for.
+	 * @return Password for user if found; otherwise returns null.
+	 */
+	private String getPasswordForUser(String username) {
+		//TODO: Search database for user's password.
+
+		// TODO: Delete hardcoded test data.
+		if("user".equals(username)){
+			return "password";
+		}
+
+		return null;
+	}
+}
---
+++ Connection/src/edu/nku/CSC440/stagbar/ui/LoginUI.java
@@ -0,0 +1,75 @@
+package edu.nku.CSC440.stagbar.ui;
+
+import edu.nku.CSC440.stagbar.service.LoginService;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LoginUI extends JDialog {
+	private JPanel contentPane;
+	private JButton buttonOK;
+	private JButton buttonCancel;
+	private JPasswordField password;
+	private JTextField username;
+	private JLabel errorMessage;
+	private static final String INVALID_LOGIN = "Invalid Username/Password.";
+	private LoginService loginService;
+
+	public LoginUI() {
+		loginService = new LoginService();
+
+		setContentPane(contentPane);
+		setModal(true);
+		setTitle("Login");
+
+		getRootPane().setDefaultButton(buttonOK);
+
+		buttonOK.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {onOK();}
+		});
+
+		buttonCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {onCancel();}
+		});
+
+		// call onCancel() when cross is clicked
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				onCancel();
+			}
+		});
+
+		// call onCancel() on ESCAPE
+		contentPane.registerKeyboardAction(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onCancel();
+			}
+		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+
+		pack();
+		setLocationRelativeTo(null);
+		setResizable(false);
+	}
+
+	private void onOK() {
+		if(loginService.login(username.getText(), password.getPassword())){
+			System.out.println("Login Successful");
+			dispose();
+		}
+
+		errorMessage.setText(INVALID_LOGIN);
+		pack();
+	}
+
+	private void onCancel() {
+		dispose();
+	}
+
+	public static void main(String[] args) {
+		LoginUI dialog = new LoginUI();
+		dialog.setVisible(true);
+		System.exit(0);
+	}
+}
