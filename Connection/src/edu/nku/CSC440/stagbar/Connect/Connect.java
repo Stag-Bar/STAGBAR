56c995fee108733d0acdf964104bdb32fa2fe8e3
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-03-03 13:51:52 -0500

    Reorder code

--- Connection/src/edu/nku/CSC440/stagbar/Connect/Connect.java
+++ Connection/src/edu/nku/CSC440/stagbar/Connect/Connect.java
@@ -3,187 +3,28 @@ package edu.nku.CSC440.stagbar.Connect;
 import edu.nku.CSC440.stagbar.Application;
 
 import java.sql.*;
+
 public class Connect {
 
 	private static final String DATABASE_URL = "jdbc:mysql://stagbar.cgef59ufduu4.us-west-2.rds.amazonaws.com:3306";
 	private static Connect connect = new Connect();
 	private Connection activeConnection;
 
-	private Connect(){}
+	private Connect() {}
 
-	public static Connect getInstance(){
+	public static Connect getInstance() {
 		return connect;
 	}
 
-	/**
-	 * Make a connection to the database.
-	 * @param username
-	 * @param password
-	 * @return Connection to database if successful, otherwise returns <code>null</code>.
-	 */
-	private Connection makeNewConnection(String username, String password) {
-		return makeNewConnection(username, password, "");
-	}
-
-	/**
-	 * Make a connection to the database.
-	 * @param username
-	 * @param password
-	 * @param dataBaseName
-	 * @return Connection to database if successful, otherwise returns <code>null</code>.
-	 * @throws RuntimeException If mysql is not setup properly.
-	 */
-	private Connection makeNewConnection(String username, String password, String dataBaseName) {
-		Connection conn;
-		try{
-			Class.forName("com.mysql.jdbc.Driver"); // Register MySQL Driver. Needed?
-			String url = DATABASE_URL + "/" + dataBaseName;
-			conn = DriverManager.getConnection(url, username, password);
-		}
-		catch(SQLException e){
-			conn = null;
-		} catch(ClassNotFoundException e){
-			throw new RuntimeException("Check mysql setup!", e);
-		}
-		return conn;
-	}
-
-	/**
-	 * @return <code>true</code> if given username & password can connect to database
-	 * @deprecated Temporary method. Unsecure.
-	 */
-	public boolean createUserConnection(String username, String password){
-		activeConnection = makeNewConnection(username, password, "test");
-		if(activeConnection != null){
-			Application.getInstance().setConnection(activeConnection);
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
-	 * @param name Name of database to create.
-	 * @return <code>true</code> if successful, <code>false</code> otherwise.
-	 */
-	public boolean createDatabase(String name){
-		Connection c;
-		boolean success = false;
-		try {
-			c = makeNewConnection("stagbar", "Nkucsc440");
-			Statement sta = c.createStatement();
-			success =  sta.execute("CREATE DATABASE " + name + ";");
-			c.close();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return success;
-	}
-
-	/**
-	 * Create a new user that has full privileges.  IE the inventory manager
-	 * @return <code>true</code> if successful, <code>false</code> otherwise.
-	 */
-	public boolean createMasterUser(String username, String password, String database) {
-		try{
-			//FIXME: Use existing connection.
-			Connection conn = makeNewConnection("stagbar", "Nkucsc440", database);//have to connect to the database with an admin account
-																		//to create a new user
-			//Statement sta = conn.createStatement();
-			boolean exists = doesUserExist(username, conn);
-			PreparedStatement pSta;
-
-			if(!exists){
-				String createUserStatement = "CREATE USER ? @'%' IDENTIFIED BY ?;";
-				pSta = conn.prepareStatement(createUserStatement);
-				pSta.setString(1, username);
-				pSta.setString(2, password);
-				pSta.execute();
-
-				//FIXME: This statement is not granting priveleges. I had to manually set priveleges in the DB to log in with a new user.
-				String grantPrivStatement = "GRANT ALL PRIVILEGES ON " + database + " TO ? @'%' IDENTIFIED BY ?;";
-				pSta = conn.prepareStatement(grantPrivStatement);
-				pSta.setString(1, username);
-				pSta.setString(2, password);
-				pSta.execute();
-				//sta.execute("create user " + "\'" + username + "\'@\'%\'" + " IDENTIFIED BY \'" + password + "\';" );
-				//sta.execute("grant all privileges on *.* to \'" + username
-					//+ "\'@\'%\' identified by \'" + password + "\';");
-				conn.close();
-				return true;
-			}
-			else {
-				conn.close();
-			}
-		}
-		catch(SQLException e){
-			System.out.println(e);
-		}
-		return false;
-	}
-
-	//TODO: Delete after we discuss maintaining connections.
-	/** @deprecated defer to public method.  */
-	private boolean doesUserExist(String username, Connection conn) throws SQLException {
-		String statement = "SELECT * FROM mysql.user WHERE user = ?;";
-		PreparedStatement pSta = conn.prepareStatement(statement);
-		pSta.setString(1, username);
-		ResultSet checkResults = pSta.executeQuery();
-		return checkResults.next();
-	}
-
-	/**
-	 * Checks database for given username.
-	 *
-	 * @param username Username to check database for.
-	 * @return <code>true</code> if given username is in the database,
-	 * <code>false</code> otherwise.
-	 * @throws RuntimeException If user's database connection is closed.
-	 */
-	public boolean doesUserExist(String username){
-		String statement = "SELECT * FROM mysql.user WHERE user = ?;";
-		try {
-			PreparedStatement pSta = activeConnection.prepareStatement(statement);
-			pSta.setString(1, username);
-			ResultSet checkResults = pSta.executeQuery();
-			return checkResults.next();
-		} catch(SQLException e) {
-			throw new RuntimeException("Connection to database has been closed.",e);
-		}
-	}
-
-	/**
-	 * Delete given user from the database.
-	 * @param username User to delete.
-	 * @return <code>true</code> if successful, <code>false</code> otherwise.
-	 */
-	protected boolean deleteUser(String username) {
-		boolean succesful = false;
-		try{
-			Connection conn = makeNewConnection("stagbar", "Nkucsc440");//have to connect as admin.
-			String statement = "DROP USER ?;";
-			PreparedStatement pSta = conn.prepareStatement(statement);
-			pSta.setString(1, username);
-			succesful = pSta.execute();
-			//Statement sta = conn.createStatement();
-			//sta.execute("DROP USER \'" + username + "\';");
-			conn.close();
-		}
-		catch(SQLException e){
-			System.out.println(e);
-		}
-		return succesful;
-	}
-
 	/** Create a table to store the different kinds of alcohol */
-	protected boolean addNewGroup(Connection conn, String groupName, int code){
+	protected boolean addNewGroup(Connection conn, String groupName, int code) {
 		boolean successful = false;
 		/* code will indicate the type
 		 * 1 = bottled beer
 		 * 2 = draft beer
 		 * 3 = liqour
 		 */
-		try{
+		try {
 			Statement sta = conn.createStatement();
 			String statement = "";
 			//PreparedStatement pSta;
@@ -204,24 +45,187 @@ public class Connect {
 			// & type is the associated string
 
 
-
-			switch (code){
-				case 1: statement = "CREATE TABLE " + groupName  + " (name varchar(50), previous integer, current integer, primary key(name));";
-						break;
-				case 2: statement = "CREATE TABLE " + groupName  + " (name varchar(50), previous double, current double, primary key(name));";
-						break;
-				case 3: statement = "CREATE TABLE " + groupName  + " (name varchar(50), previous double, current double, fullBottles integer, primary key (name));";
-						break;
+			switch(code) {
+				case 1:
+					statement = "CREATE TABLE " + groupName + " (name varchar(50), previous integer, current integer, primary key(name));";
+					break;
+				case 2:
+					statement = "CREATE TABLE " + groupName + " (name varchar(50), previous double, current double, primary key(name));";
+					break;
+				case 3:
+					statement = "CREATE TABLE " + groupName + " (name varchar(50), previous double, current double, fullBottles integer, primary key (name));";
+					break;
 			}
 			//pSta = conn.prepareStatement(statement);
 			//pSta.setString(1, groupName);
 			successful = sta.execute(statement);
 			//successful = pSta.execute();
-		}
-		catch(Exception e){
+		} catch(Exception e) {
 			System.out.println(e);
 		}
 		return successful;
+	}
+
+	/**
+	 * @param name Name of database to create.
+	 * @return <code>true</code> if successful, <code>false</code> otherwise.
+	 */
+	public boolean createDatabase(String name) {
+		Connection c;
+		boolean success = false;
+		try {
+			c = makeNewConnection("stagbar", "Nkucsc440");
+			Statement sta = c.createStatement();
+			success = sta.execute("CREATE DATABASE " + name + ";");
+			c.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return success;
+	}
+
+	/**
+	 * Create a new user that has full privileges.  IE the inventory manager
+	 *
+	 * @return <code>true</code> if successful, <code>false</code> otherwise.
+	 */
+	public boolean createMasterUser(String username, String password, String database) {
+		try {
+			//FIXME: Use existing connection.
+			Connection conn = makeNewConnection("stagbar", "Nkucsc440", database);//have to connect to the database with an admin account
+			//to create a new user
+			//Statement sta = conn.createStatement();
+			boolean exists = doesUserExist(username, conn);
+			PreparedStatement pSta;
+
+			if(!exists) {
+				String createUserStatement = "CREATE USER ? @'%' IDENTIFIED BY ?;";
+				pSta = conn.prepareStatement(createUserStatement);
+				pSta.setString(1, username);
+				pSta.setString(2, password);
+				pSta.execute();
+
+				//FIXME: This statement is not granting priveleges. I had to manually set priveleges in the DB to log in with a new user.
+				String grantPrivStatement = "GRANT ALL PRIVILEGES ON " + database + " TO ? @'%' IDENTIFIED BY ?;";
+				pSta = conn.prepareStatement(grantPrivStatement);
+				pSta.setString(1, username);
+				pSta.setString(2, password);
+				pSta.execute();
+				//sta.execute("create user " + "\'" + username + "\'@\'%\'" + " IDENTIFIED BY \'" + password + "\';" );
+				//sta.execute("grant all privileges on *.* to \'" + username
+				//+ "\'@\'%\' identified by \'" + password + "\';");
+				conn.close();
+				return true;
+			}
+			else {
+				conn.close();
+			}
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
+		return false;
+	}
+
+	/**
+	 * @return <code>true</code> if given username & password can connect to database
+	 * @deprecated Temporary method. Unsecure.
+	 */
+	public boolean createUserConnection(String username, String password) {
+		activeConnection = makeNewConnection(username, password, "test");
+		if(activeConnection != null) {
+			Application.getInstance().setConnection(activeConnection);
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Delete given user from the database.
+	 *
+	 * @param username User to delete.
+	 * @return <code>true</code> if successful, <code>false</code> otherwise.
+	 */
+	protected boolean deleteUser(String username) {
+		boolean succesful = false;
+		try {
+			Connection conn = makeNewConnection("stagbar", "Nkucsc440");//have to connect as admin.
+			String statement = "DROP USER ?;";
+			PreparedStatement pSta = conn.prepareStatement(statement);
+			pSta.setString(1, username);
+			succesful = pSta.execute();
+			//Statement sta = conn.createStatement();
+			//sta.execute("DROP USER \'" + username + "\';");
+			conn.close();
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
+		return succesful;
+	}
+
+	//TODO: Delete after we discuss maintaining connections.
+
+	/** @deprecated defer to public method. */
+	private boolean doesUserExist(String username, Connection conn) throws SQLException {
+		String statement = "SELECT * FROM mysql.user WHERE user = ?;";
+		PreparedStatement pSta = conn.prepareStatement(statement);
+		pSta.setString(1, username);
+		ResultSet checkResults = pSta.executeQuery();
+		return checkResults.next();
+	}
+
+	/**
+	 * Checks database for given username.
+	 *
+	 * @param username Username to check database for.
+	 * @return <code>true</code> if given username is in the database,
+	 * <code>false</code> otherwise.
+	 * @throws RuntimeException If user's database connection is closed.
+	 */
+	public boolean doesUserExist(String username) {
+		String statement = "SELECT * FROM mysql.user WHERE user = ?;";
+		try {
+			PreparedStatement pSta = activeConnection.prepareStatement(statement);
+			pSta.setString(1, username);
+			ResultSet checkResults = pSta.executeQuery();
+			return checkResults.next();
+		} catch(SQLException e) {
+			throw new RuntimeException("Connection to database has been closed.", e);
+		}
+	}
+
+	/**
+	 * Make a connection to the database.
+	 *
+	 * @param username
+	 * @param password
+	 * @param dataBaseName
+	 * @return Connection to database if successful, otherwise returns <code>null</code>.
+	 * @throws RuntimeException If mysql is not setup properly.
+	 */
+	private Connection makeNewConnection(String username, String password, String dataBaseName) {
+		Connection conn;
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); // Register MySQL Driver. Needed?
+			String url = DATABASE_URL + "/" + dataBaseName;
+			conn = DriverManager.getConnection(url, username, password);
+		} catch(SQLException e) {
+			conn = null;
+		} catch(ClassNotFoundException e) {
+			throw new RuntimeException("Check mysql setup!", e);
+		}
+		return conn;
+	}
+
+	/**
+	 * Make a connection to the database.
+	 *
+	 * @param username
+	 * @param password
+	 * @return Connection to database if successful, otherwise returns <code>null</code>.
+	 */
+	private Connection makeNewConnection(String username, String password) {
+		return makeNewConnection(username, password, "");
 	}
 
 }
