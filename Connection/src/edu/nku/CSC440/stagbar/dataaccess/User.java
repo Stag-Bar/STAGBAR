892b8faf752dfdc76d5d7b2683c095eca105a56f
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-04-07 10:58:18 -0400

    Allow User to check for equality

--- Connection/src/edu/nku/CSC440/stagbar/dataaccess/User.java
+++ Connection/src/edu/nku/CSC440/stagbar/dataaccess/User.java
@@ -10,12 +10,27 @@ public class User {
 		this.permissionLevel = permissionLevel;
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+
+		User user = (User)o;
+
+		return getUsername().equals(user.getUsername());
+	}
+
 	public PermissionLevel getPermissionLevel() {
 		return permissionLevel;
 	}
 
 	public String getUsername() {
 		return username;
+	}
+
+	@Override
+	public int hashCode() {
+		return getUsername().hashCode();
 	}
 
 	public boolean isAdmin(){
