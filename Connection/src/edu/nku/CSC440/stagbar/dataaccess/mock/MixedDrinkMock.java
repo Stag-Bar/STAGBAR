40bf3d3fb7cb5d218e7ba735fdf4e5df40a71e47
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-04-07 10:57:23 -0400

    Correct Mixed Drink mocks to account for changes to mixed drinks.

--- Connection/src/edu/nku/CSC440/stagbar/dataaccess/mock/MixedDrinkMock.java
+++ Connection/src/edu/nku/CSC440/stagbar/dataaccess/mock/MixedDrinkMock.java
@@ -3,14 +3,14 @@ package edu.nku.CSC440.stagbar.dataaccess.mock;
 import edu.nku.CSC440.stagbar.dataaccess.MixedDrink;
 import edu.nku.CSC440.stagbar.dataaccess.MixedDrinkIngredient;
 
-import java.util.HashSet;
+import java.time.LocalDate;
 
 public class MixedDrinkMock {
-	public static final MixedDrink FIREBULL = new MixedDrink("Firebull", new HashSet<MixedDrinkIngredient>() {{
-		add(new MixedDrinkIngredient(AlcoholMock.WHISKEY, 2.0));
-	}}, null);
+	public static final MixedDrink FIREBULL = new MixedDrink("Firebull", (LocalDate)null) {{
+		addIngredientFromDatabase(new MixedDrinkIngredient(AlcoholMock.WHISKEY, 2.0));
+	}};
 
-	public static final MixedDrink LONG_ISLAND = new MixedDrink("Long Island Iced Tea", new HashSet<MixedDrinkIngredient>() {{
-		add(new MixedDrinkIngredient(AlcoholMock.VODKA, 1.0));
-	}}, null);
+	public static final MixedDrink LONG_ISLAND = new MixedDrink("Long Island Iced Tea", (LocalDate)null) {{
+		addIngredientFromDatabase(new MixedDrinkIngredient(AlcoholMock.VODKA, 1.0));
+	}};
 }
