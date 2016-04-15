52644f25468208853d2de335eb0808625792ed5f
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-04-15 16:10:50 -0400

    Replace anonymous classes in mock with builder. Fixes test comparisons.

--- Connection/src/edu/nku/CSC440/stagbar/dataaccess/mock/MixedDrinkMock.java
+++ Connection/src/edu/nku/CSC440/stagbar/dataaccess/mock/MixedDrinkMock.java
@@ -4,11 +4,30 @@ import edu.nku.CSC440.stagbar.dataaccess.MixedDrink;
 import edu.nku.CSC440.stagbar.dataaccess.MixedDrinkIngredient;
 
 public class MixedDrinkMock {
-	public static final MixedDrink FIREBULL = new MixedDrink("Firebull", false) {{
-		addIngredientFromDatabase(new MixedDrinkIngredient(AlcoholMock.WHISKEY, 2.0));
-	}};
+	public static final MixedDrink FIREBULL = new MixedDrinkBuilder("Firebull", false)
+			.addIngredientFromDatabase(new MixedDrinkIngredient(AlcoholMock.WHISKEY, 2.0))
+			.build();
 
-	public static final MixedDrink LONG_ISLAND = new MixedDrink("Long Island Iced Tea", false) {{
-		addIngredientFromDatabase(new MixedDrinkIngredient(AlcoholMock.VODKA, 1.0));
-	}};
+	public static final MixedDrink LONG_ISLAND = new MixedDrinkBuilder("Long Island Iced Tea", false)
+			.addIngredientFromDatabase(new MixedDrinkIngredient(AlcoholMock.VODKA, 1.0))
+			.build();
+
+	private static class MixedDrinkBuilder {
+		MixedDrink mixedDrink;
+
+		public MixedDrinkBuilder(String name, boolean isRetired) {
+			mixedDrink = new MixedDrink(name, isRetired);
+		}
+
+		public MixedDrinkBuilder addIngredientFromDatabase(MixedDrinkIngredient ingredient) {
+			mixedDrink.addIngredientFromDatabase(ingredient);
+			return this;
+		}
+
+		public MixedDrink build() {
+			return mixedDrink;
+		}
+
+
+	}
 }
