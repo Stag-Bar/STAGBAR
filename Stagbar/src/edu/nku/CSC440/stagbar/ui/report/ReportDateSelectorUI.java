5c11c5b6e276c1a4a30f3e93dcd55ae1e48c8bb0
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-04-17 21:47:24 -0400

    Begin date selector for reports.

---
+++ Stagbar/src/edu/nku/CSC440/stagbar/ui/report/ReportDateSelectorUI.java
@@ -0,0 +1,18 @@
+package edu.nku.CSC440.stagbar.ui.report;
+
+import javafx.embed.swing.JFXPanel;
+
+import javax.swing.*;
+
+public class ReportDateSelectorUI {
+	private JButton cancelButton;
+	private JPanel contentPane;
+	private JFXPanel endDateJFXPanel;
+	private JButton okButton;
+	private JFXPanel startDateJFXPanel;
+
+	ReportDateSelectorUI() {
+
+	}
+
+}
