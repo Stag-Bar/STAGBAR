9d3779625da9163590f3f8f98e85901e3f5f519e
dudarg1 <dudarg1@nku.edu>
2016-04-25 22:14:29 -0400

    Groups add successfully

--- Stagbar/src/edu/nku/CSC440/stagbar/ui/alcohol/CreateCustomGroupUI.java
+++ Stagbar/src/edu/nku/CSC440/stagbar/ui/alcohol/CreateCustomGroupUI.java
@@ -2,40 +2,41 @@ package edu.nku.CSC440.stagbar.ui.alcohol;
 
 import javax.swing.*;
 
+import edu.nku.CSC440.stagbar.dataaccess.data.AlcoholType;
+import edu.nku.CSC440.stagbar.dataaccess.data.CustomAlcoholType;
+import edu.nku.CSC440.stagbar.dataaccess.databaseConnection.Connect;
+import edu.nku.CSC440.stagbar.service.AlcoholService;
+import edu.nku.CSC440.stagbar.ui.common.uiHacks;
+
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.Set;
 
 
 public class CreateCustomGroupUI {
 	
-	
-	
-	private JTextField textField;
 	private JPanel mainpanel;
+	private JTextField nameField;
+	private JComboBox box;
 	
 	public CreateCustomGroupUI(){
-		
-	
-		
-		
+				
 		mainpanel = new JPanel();
-		//getContentPane().add(mainpanel);
-		mainpanel.setLayout(new GridLayout(2, 2, 100, 100));
+		mainpanel.setLayout(new GridLayout(3, 2, 100, 100));
 		
 		mainpanel.setName("Custom Group");
 		mainpanel.setVisible(true);
-		//mainpanel.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+				
 		JPanel panel1 = new JPanel();
 		mainpanel.add(panel1);
 		JLabel lblAlcoholName = new JLabel("Alcohol Name");
 		panel1.add(lblAlcoholName);
 		
 		JPanel panel2 = new JPanel();
-		textField = new JTextField();
+		nameField = new JTextField();
 		mainpanel.add(panel2);
-		panel2.add(textField);
-		textField.setColumns(10);
+		panel2.add(nameField);
+		nameField.setColumns(10);
 		
 		JPanel panel3 = new JPanel();
 		mainpanel.add(panel3);
@@ -45,12 +46,49 @@ public class CreateCustomGroupUI {
 		JPanel panel4 = new JPanel();
 		mainpanel.add(panel4);
 		String[] options = {"Bottled","Draft","Shelf"}; 
-		JComboBox box = new JComboBox(options);
+		box = new JComboBox<String>(options);
 		panel4.add(box);
 		mainpanel.add(panel4);
 		
+		JPanel panel5 = new JPanel();
+		mainpanel.add(panel5);
+		JButton back = new JButton("Back");
+		back.addActionListener(e -> onBack());
+		panel5.add(back);
 		
+		JPanel panel6 = new JPanel();
+		mainpanel.add(panel6);
+		JButton submit = new JButton("Submit");
+		submit.addActionListener(e -> onSubmit());
+		panel6.add(submit);
+				
 	}
+			
+	private void onBack(){
+		uiHacks.killMeThenGoToLastPage(mainpanel);
+	}
+	
+	private void onSubmit(){
+		
+		CustomAlcoholType newGroup;
+		Set set = Connect.getInstance().findAllCustomAlcoholTypes();
+		int index = set.size();
+		String name = nameField.getText();
+		
+		if ((String)box.getSelectedItem() == "Bottled"){
+			newGroup = new CustomAlcoholType(index, name, AlcoholType.BOTTLED);
+		}
+		else if ((String)box.getSelectedItem() == "Draft"){
+			newGroup = new CustomAlcoholType(index, name, AlcoholType.DRAFT);
+		}
+		else{
+			newGroup = new CustomAlcoholType(index, name, AlcoholType.SHELF);
+		}
+		
+		Connect.getInstance().saveCustomAlcoholType(newGroup);
+				
+	}
+		
 	
 	public JPanel getContentPane() {
         return mainpanel;
